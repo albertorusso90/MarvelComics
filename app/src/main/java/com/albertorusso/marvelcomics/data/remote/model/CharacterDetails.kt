561f226881b6efb9713815dd334b0f665f3aacda
albertorusso1990 <albertorusso1990@gmail.com>
2023-12-27 22:08:54 +0100

    remove unnecesary attributes

--- app/src/main/java/com/albertorusso/marvelcomics/data/remote/model/CharacterDetails.kt
+++ app/src/main/java/com/albertorusso/marvelcomics/data/remote/model/CharacterDetails.kt
@@ -4,10 +4,5 @@ data class CharacterDetails(
     val id: Int,
     val name: String,
     val description: String,
-    val resourceURI: String,
-    val thumbnail: Thumbnail,
-    val comics: ComicList,
-    val stories: StoryList,
-    val events: EventList,
-    val series: SeriesList
+    val thumbnail: Thumbnail
 )
